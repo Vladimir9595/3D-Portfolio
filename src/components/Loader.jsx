5fd84f4d7f39fca59bcf27c8d5243c90f961ec72
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-03-13 21:25:30 +0100

    add: Loader section (#4)

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Html, useProgress } from '@react-three/drei'
 
 const Loader = () => {
+  const { progress } = useProgress()
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 12,
+          color: '#373737',
+          fontWeight: 800,
+          marginTop: 180,
+          marginLeft: -22,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
   )
 }
 
