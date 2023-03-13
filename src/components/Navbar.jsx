41d288e72a083895041c5bae521cd7688a755953
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-03-13 21:24:50 +0100

    update: Navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-[#232323]`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -21,9 +21,9 @@ const Navbar = () => {
             window.scrollTo(0, 0)
           }}
         >
-          <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Vladimir &nbsp;<span className="sm:block hidden">| Sacchetto </span>
+          <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
+          <p className="text-white text-[17px] font-bold cursor-pointer flex">
+            Vladimir&nbsp;<span className="sm:block hidden">| Sacchetto </span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
