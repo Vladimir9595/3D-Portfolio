25de220ea421d23e5cb1d6de545c7c5c9f1cb664
Vladimir9595 <alss-sio-sisr21-svl@ccicampus.fr>
2023-03-14 16:19:39 +0100

    update: styles (#11)

--- src/styles.js
+++ src/styles.js
@@ -4,14 +4,14 @@ const styles = {
   padding: 'sm:px-16 px-6 sm:py-16 py-10',
 
   heroHeadText:
-    'font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2',
+    'font-black text-[#373737] lg:text-[80px] sm:text-[55px] xs:text-[45px] text-[35px] lg:leading-[98px] mt-2',
   heroSubText:
-    'text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]',
+    'text-[#373737] font-medium lg:text-[30px] sm:text-[22px] xs:text-[20px] text-[16px] lg:leading-[40px]',
 
   sectionHeadText:
-    'text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]',
+    'text-[#373737] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]',
   sectionSubText:
-    'sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider',
+    'sm:text-[20px] text-[15px] text-[#232323] uppercase tracking-wider',
 }
 
 export { styles }
