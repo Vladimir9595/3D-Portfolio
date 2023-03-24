7d59e656fa8f07b628c67cc8bdef5cf511817612
Vladimir9595 <vladisac@hotmail.it>
2023-03-24 03:08:28 +0100

    fix: About text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,11 +19,12 @@ const About = () => {
         className="mt-5 text-[#373737] text-[17px] max-w-3xl leading-[30px]"
       >
         I am a serious and invested person, ready to assume missions as a web
-        developer for the maintenance and management of sites. I'm currently
-        working for a BTS SIO within the CCS - SAT Clients belonging to the
-        Crédit Mutuel Alliance Fédérale group, I continue my professional and
-        professional and personal development through self-training on
-        programming languages on programming languages and new technologies.
+        developer for the maintenance and management of sites. <br /> I'm
+        currently working for a BTS SIO within the CCS - SAT Clients belonging
+        to the Crédit Mutuel Alliance Fédérale group. <br /> I continue my
+        professional and professional and personal development through
+        self-training on programming languages on programming languages and new
+        technologies.
       </motion.p>
     </>
   )
