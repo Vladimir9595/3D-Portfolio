a4530b22ce6912e643d02e569688efbf49d0990e
Vladimir9595 <vladisac@hotmail.it>
2023-03-26 18:54:19 +0200

    fix: Earth canvas (#33)

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -20,7 +20,7 @@ const Earth = ({ isMobile }) => {
       />
       <primitive
         object={earth.scene}
-        scale={isMobile ? 0.018 : 0.023}
+        scale={isMobile ? 0.02 : 0.024}
         position={isMobile ? [-0.1, -2.15, 0.1] : [-0.1, -2.15, 0.1]}
         rotation={[0.02, -0.45, -0.5]}
       />
@@ -53,25 +53,31 @@ const EarthCanvas = () => {
   }, [])
 
   return (
-    <Canvas
-      frameLoop="demand"
-      shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate={true}
-          autoRotateSpeed={isMobile ? 0.5 : 0.5}
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Earth />
-      </Suspense>
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Canvas
+        style={{
+          width: isMobile ? '100vw' : '100vw',
+          height: isMobile ? '100vh' : '100vh',
+        }}
+        frameLoop="demand"
+        shadows
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            autoRotate={true}
+            autoRotateSpeed={isMobile ? 0.5 : 0.5}
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Earth isMobile={isMobile} />
+        </Suspense>
 
-      <Preload all />
-    </Canvas>
+        <Preload all />
+      </Canvas>
+    </div>
   )
 }
 
