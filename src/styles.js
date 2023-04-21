70dbc19bed6d2bef28fb186b5ca6ac4b4e975214
Vladimir9595 <vladisac@hotmail.it>
2023-04-21 19:04:39 +0200

    fix: update style (#11)

--- src/styles.js
+++ src/styles.js
@@ -3,15 +3,15 @@ const styles = {
   paddingY: 'sm:py-16 py-6',
   padding: 'sm:px-16 px-6 sm:py-16 py-10',
 
-  heroHeadText:
-    'font-black text-[#373737] lg:text-[80px] sm:text-[55px] xs:text-[45px] text-[35px] lg:leading-[98px] mt-2',
+  heroHeadTitle:
+    'font-black text-[#373737] lg:text-[60px] sm:text-[45px] xs:text-[35px] text-[25px] lg:leading-[98px] mt-2',
   heroSubText:
-    'text-[#373737] font-medium lg:text-[30px] sm:text-[22px] xs:text-[20px] text-[16px] lg:leading-[40px]',
+    'text-[#373737] font-medium lg:text-[25px] sm:text-[18px] xs:text-[15px] text-[13px] lg:leading-[40px] mt-2',
 
   sectionHeadText:
     'text-[#373737] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]',
   sectionSubText:
-    'sm:text-[20px] text-[15px] text-[#232323] uppercase tracking-wider',
+    'text-[#232323] font-semibold sm:text-[20px] text-[15px] uppercase tracking-wider',
 }
 
 export { styles }
