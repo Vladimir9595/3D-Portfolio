80902c9a310d1a67ff0b89881e22041ef31ee3e6
Vladimir9595 <vladisac@hotmail.it>
2023-10-11 13:13:48 +0200

    update: text About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,11 +33,14 @@ const About = () => {
         className="mt-5 text-[#373737] text-justify sm:text-[18px] text-[13px] max-w-3xl leading-[30px]"
       >
         I am a serious and invested person, ready to assume missions as a web
-        developer for the maintenance and management of sites. <br /> I'm
-        currently working for a BTS SIO within the CCS - SAT Clients belonging
-        to the Crédit Mutuel Alliance Fédérale group. <br /> I continue my
-        professional and personal development through self-training on
-        programming languages and new technologies.
+        developer for the maintenance and management of sites. <br />
+        In July 2023, I obtained my BTS SIO at the CCI Campus in Strasbourg,
+        thanks to a work-study programme with CCS - SAT Clients, part of the
+        Crédit Mutuel Alliance Fédérale group. <br /> In September 2023, I
+        joined the CNAM in Strasbourg to continue my studies for the Diplôme
+        d'Ingénieur Informatique, specialising in Information Systems. This
+        course is run in partnership with ITII Alsace and is a sandwich course
+        at Alcatel-Lucent Enterprise.
       </motion.p>
     </>
   )
