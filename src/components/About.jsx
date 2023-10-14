59d3679e35a4af2de04c52dd79944bafe15b22f8
Vladimir9595 <vladisac@hotmail.it>
2023-10-14 16:32:20 +0200

    fix: text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,17 +30,30 @@ const About = () => {
 
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
-        className="mt-5 text-[#373737] text-justify sm:text-[18px] text-[13px] max-w-3xl leading-[30px]"
+        className="mt-5 text-[#373737] text-justify sm:text-[18px] text-[13px] max-w-4xl leading-[30px]"
       >
-        I am a serious and invested person, ready to assume missions as a web
-        developer for the maintenance and management of sites. <br />
+        I am a dedicated and committed individual, prepared to undertake roles
+        as a web developer, specializing in website maintenance and management.{' '}
+        <br />
+        <br />
         In July 2023, I obtained my BTS SIO at the CCI Campus in Strasbourg,
         thanks to a work-study programme with CCS - SAT Clients, part of the
-        Crédit Mutuel Alliance Fédérale group. <br /> In September 2023, I
-        joined the CNAM in Strasbourg to continue my studies for the Diplôme
-        d'Ingénieur Informatique, specialising in Information Systems. This
-        course is run in partnership with ITII Alsace and is a sandwich course
-        at Alcatel-Lucent Enterprise.
+        Crédit Mutuel Alliance Fédérale group. <br />
+        <br /> Furthermore, in September 2023, I enrolled at CNAM in Strasbourg
+        to pursue my studies toward a Computer Engineering Diploma, with a
+        specialization in Information Systems. This educational program is
+        conducted in collaboration with ITII Alsace and encompasses an
+        apprenticeship at Alcatel-Lucent Enterprise. <br />
+        <br />
+        Should you require additional information or have any further questions,
+        please do not hesitate to contact me.
+        {/*  I am a dedicated and committed individual, prepared to undertake roles as a web developer, specializing in website maintenance and management.
+
+In July 2023, I successfully completed my BTS SIO program at CCI Campus in Strasbourg, through a work-study arrangement with CCS - SAT Clients, a division of the Crédit Mutuel Alliance Fédérale group.
+
+Furthermore, in September 2023, I enrolled at CNAM in Strasbourg to pursue my studies toward a Computer Engineering Diploma, with a specialization in Information Systems. This educational program is conducted in collaboration with ITII Alsace and encompasses an apprenticeship at Alcatel-Lucent Enterprise.
+
+Should you require additional information or have any further questions, please do not hesitate to contact me. */}
       </motion.p>
     </>
   )
