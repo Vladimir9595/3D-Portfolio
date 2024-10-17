56b2bba71616d3a26d6e5722cd2e236de10f2060
Vladimir9595 <vladisac@hotmail.it>
2024-10-17 12:13:40 +0200

    feat: add news handler (#20)

--- src/components/NewsHandler.jsx
+++ src/components/NewsHandler.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const NewsHandler = () => {
   const [news, setNews] = useState([])
-  const [newProjectName, setNewProjectName] = useState('')
-  const [isEditingProject, setIsEditingProject] = useState(false)
-  const [editingProjectId, setEditingProjectId] = useState(null)
-  const [editingProjectName, setEditingProjectName] = useState('')
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetchNews()
@@ -21,49 +19,9 @@ const NewsHandler = () => {
     }
   }
 
-  const handleAddProject = async () => {
-    if (newProjectName.trim() !== '') {
-      try {
-        await axios.post('http://172.17.166.31:5000/api/news', {
-          name: newProjectName,
-        })
-        setNewProjectName('')
-        fetchNews()
-      } catch (error) {
-        console.error('Error adding news:', error)
-      }
-    }
-  }
-
-  const handleEditProject = (projectId) => {
-    setIsEditingProject(true)
-    setEditingProjectId(projectId)
-    const projectToEdit = news.find((project) => project.id === projectId)
-    setEditingProjectName(projectToEdit.name)
-  }
-
-  const handleUpdateProject = async () => {
-    if (editingProjectName.trim() !== '') {
-      try {
-        await axios.put(
-          `http://172.17.166.31:5000/api/news/${editingProjectId}`,
-          {
-            name: editingProjectName,
-          },
-        )
-        setIsEditingProject(false)
-        setEditingProjectId(null)
-        setEditingProjectName('')
-        fetchNews()
-      } catch (error) {
-        console.error('Error updating news:', error)
-      }
-    }
-  }
-
-  const handleDeleteProject = async (projectId) => {
+  const handleDeleteNews = async (newsId) => {
     try {
-      await axios.delete(`http://172.17.166.31:5000/api/news/${projectId}`)
+      await axios.delete(`http://172.17.166.31:5000/api/news/${newsId}`)
       fetchNews()
     } catch (error) {
       console.error('Error deleting news:', error)
@@ -71,64 +29,40 @@ const NewsHandler = () => {
   }
 
   return (
-    <div className="container mx-auto mt-8">
-      <h2 className="text-3xl font-bold mb-4">News</h2>
-      <div className="mb-4">
-        <input
-          type="text"
-          value={newProjectName}
-          onChange={(e) => setNewProjectName(e.target.value)}
-          placeholder="Enter new project name"
-          className="border rounded-md p-2"
-        />
-        <button
-          onClick={handleAddProject}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2"
-        >
-          Add News
-        </button>
+    <div className="container pl-[20px] mt-10">
+      <div className="md:block flex gap-4 md:pt-[0px] sm:pt-[12px] pt-[10px]">
+        <h1 className="text-[#373737] font-black md:text-[2.5rem] sm:text-[2rem] xs:text-[1rem] text-[1rem] uppercase">
+          News
+        </h1>
+        <div className="mb-4">
+          <button
+            onClick={() => navigate('/add-news')}
+            className="bg-[#ffffffee] md:py-3 sm:py-2 py-2 md:px-8 sm:px-5 px-3 rounded-xl outline-none md:text-[1rem] sm:text-[0.75rem] text-[0.50rem] text-[#373737] font-bold shadow-md shadow-primary hover:bg-[#373737] hover:text-[#ffffffee] cursor-pointer"
+          >
+            Add News
+          </button>
+        </div>
       </div>
-      <ul className="list-disc">
-        {news.map((project) => (
-          <li key={project.id} className="mb-2">
-            {isEditingProject && editingProjectId === project.id ? (
-              <div className="flex items-center">
-                <input
-                  type="text"
-                  value={editingProjectName}
-                  onChange={(e) => setEditingProjectName(e.target.value)}
-                  className="border rounded-md p-2 mr-2"
-                />
-                <button
-                  onClick={handleUpdateProject}
-                  className="bg-green-500 text-white px-4 py-2 rounded-md"
-                >
-                  Update
-                </button>
-                <button
-                  onClick={() => setIsEditingProject(false)}
-                  className="bg-red-500 text-white px-4 py-2 rounded-md ml-2"
-                >
-                  Cancel
-                </button>
-              </div>
-            ) : (
-              <div className="flex items-center">
-                <span>{project.name}</span>
-                <button
-                  onClick={() => handleEditProject(project.id)}
-                  className="bg-yellow-500 text-white px-4 py-2 rounded-md ml-2"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => handleDeleteProject(project.id)}
-                  className="bg-red-500 text-white px-4 py-2 rounded-md ml-2"
-                >
-                  Delete
-                </button>
-              </div>
-            )}
+      <ul className="list-disc list-none">
+        {news.map((newsItem) => (
+          <li key={newsItem.id} className="mb-2">
+            <div className="flex items-center gap-5">
+              <span className="text-[#373737] font-black md:text-[1.2rem] sm:text-[1rem] xs:text-[0.5rem] text-[0.5rem]">
+                {newsItem.name}
+              </span>
+              <button
+                onClick={() => navigate(`/edit-news/${newsItem.id}`)}
+                className="bg-[#ffffffee] md:py-3 sm:py-2 py-2 md:px-8 sm:px-5 px-3 rounded-xl outline-none md:text-[1rem] sm:text-[0.75rem] text-[0.50rem] text-[#373737] font-bold shadow-md shadow-primary hover:bg-[#373737] hover:text-[#ffffffee] cursor-pointer"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => handleDeleteNews(newsItem.id)}
+                className="bg-[#ffffffee] md:py-3 sm:py-2 py-2 md:px-8 sm:px-5 px-3 rounded-xl outline-none md:text-[1rem] sm:text-[0.75rem] text-[0.50rem] text-[#373737] font-bold shadow-md shadow-primary hover:bg-[#373737] hover:text-[#ffffffee] cursor-pointer"
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
